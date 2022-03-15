bddde297168da666a5631c216ee9d69e8737b381
Max <89795178+maxmandia@users.noreply.github.com>
2022-03-15 11:47:49 -0400

    Manipulated the DOM in js

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+/* NEW STUFFS */ 
+
+const para = document.createElement('p')
+para.textContent = "Hey I'm red!"
+para.style.color = "red"
+
+container.appendChild(para);
+
+const h3 = document.createElement('h3')
+h3.textContent = "I'm a blue h3!"
+h3.style.color = "blue"
+
+container.appendChild(h3);
+
+const div = document.createElement('div2')
+div2.style.cssText = 'border-color: black; background-color: pink'
+
+content.appendChild(div2)
+
+const div = document.createElement('h1')
+h1.textContent = "I'm in a div!"
+
+div2.appendChild('h1')
+
+const div = document.createElement('p')
+p.textContent = "Me too!"
+
+div2.appendChild('p')
